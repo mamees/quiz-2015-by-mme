ed85d38b5bda01647b5fcf2ac2560f2508b56000
Manuel Merino Escribano <manuel.me@gmail.com>
2015-07-29 19:40:10 +0200

    ejercicio p2p obligatorio Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,51 @@
+var models = require('../models/models.js');
+
+//Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId =' + quizId));
+			}
+		}).catch(function(error){ next(error);});
+}
+
+//GET /quizes
+exports.index = function(req, res) {
+	var search = req.query.search;
+
+	if (search) {
+		search = '%'+search.replace(/s/g, "%")+'%';
+	} else {
+		// En caso que no se introduzcan datos
+		search = '%';
+	}
+	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes});
+	}).catch(function(error){ next(error);});
+
+};
+
 //GET /quizes/question
-
-exports.question = function(req, res) {
-  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+exports.show = function(req, res) {	
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
+//GET /quizes/answer
 exports.answer = function(req, res) {
-  if (req.query.respuesta === 'Roma') {
-    res.render('quizes/answer', {respuesta: 'Correcto'});
-  } else {
-    res.render('quizes/answer', {respuesta: 'Incorrecto'});
-  }
+	var resultado = 'Incorrecto';
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+	  if (req.query.respuesta === req.quiz.respuesta) {
+	    resultado = 'Correcto';
+	  }
+	  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	})
 };
 
+//GET /author
 exports.author = function(req, res) {
 	res.render('author',{author: {name: 'Manuel Merino Escribano', pathImgAuthor: 'author-photo.jpg'}})
 };
